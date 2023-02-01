d35c2846c96236abc20588a849d2b95f66e90ab6
gabinwilliams <scoutsnknives@gmail.com>
2023-01-31 22:29:10 -0600

    updated typings file

--- typings.d.ts
+++ typings.d.ts
@@ -30,6 +30,38 @@ export interface PageInfo extends SanityBody {
     profilePic: Image;
 }
 
+export interface Technology extends SanityBody {
+    _type: "skill";
+    image: Image;
+    title: string;
+}
+
+export interface Skill extends SanityBody {
+    _type: "skill";
+    image: Image;
+    title: string;
+}
+
+export interface Experience extends SanityBody {
+    _type: "experience";
+    company: string;
+    companyImage: Image;
+    dateStarted: date;
+    dateEnded: date;
+    isCurrentlyWorkingHere: boolean;
+    jobTitle: string;
+    points: string[];
+    technologies: Technology[];
+}
+
+export interface Project extends SanityBody {
+    _type: "project";
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technologies: Technology[];
+}
+
 export interface Social extends SanityBody {
     _type: "social";
     title: string;
