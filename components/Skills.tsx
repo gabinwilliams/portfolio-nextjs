1fe353266bd7c4bdcdae9d8853c02e33db1079fd
gabinwilliams <scoutsnknives@gmail.com>
2023-02-07 22:41:42 -0600

    skills css changes

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,15 +13,15 @@ function Skills({ skills }: Props) {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className="relative mx-auto flex h-screen min-h-screen max-w-[2000px] flex-col items-center justify-center overflow-hidden text-center md:text-left xl:flex-row xl:space-y-0 xl:px-10"
+            className="relative mx-auto flex h-screen min-h-screen max-w-[2000px] flex-col items-center justify-center text-center md:text-left xl:flex-row xl:px-10"
         >
             <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500">
                 Skills
             </h3>
             <h3 className="absolute top-36 text-sm uppercase tracking-[3px] text-gray-500">
-                This list continues to grow and expand!
+                The list continues to grow and expand!
             </h3>
-            <div className="grid grid-cols-4 gap-5">
+            <div className="mt-[50px] grid grid-cols-4 gap-5 p-5 md:mt-[100px] xl:mt-[200px]">
                 {skills?.slice(0, skills.length / 2).map((skill) => (
                     <TechSkill key={skill._id} skill={skill} />
                 ))}
